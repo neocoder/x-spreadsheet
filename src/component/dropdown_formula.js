2061d856f6bad6982a474175bcbc4657db9cc057
Aliaksei Ladyha <aliaksei_ladyha@epam.com>
2020-10-27 17:36:17 +0300

    fix inserting of formular from DD menu

--- src/component/dropdown_formula.js
+++ src/component/dropdown_formula.js
@@ -1,16 +1,20 @@
+import { SUPPORTED_FORMULAS } from 'hot-formula-parser';
+
 import Dropdown from './dropdown';
 import Icon from './icon';
 import { h } from './element';
 import { cssPrefix } from '../config';
-
-import { SUPPORTED_FORMULAS } from 'hot-formula-parser';
 
 export default class DropdownFormula extends Dropdown {
   constructor() {
     const nformulas = SUPPORTED_FORMULAS.map(it => h('div', `${cssPrefix}-item`)
       .on('click', () => {
         this.hide();
-        this.change(it);
+        this.change({
+          key: it,
+          title: it,
+          render: () => `=${it}()`,
+        });
       })
       .child(it));
     super(new Icon('formula'), '180px', true, 'bottom-left', ...nformulas);
