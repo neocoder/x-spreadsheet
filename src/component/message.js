771c598302eb8c03aad96b37d372a135c8280988
Aliaksei Ladyha <aliaksei_ladyha@epam.com>
2020-10-28 15:03:48 +0300

    fix backdrop for warning modal

--- src/component/message.js
+++ src/component/message.js
@@ -3,12 +3,15 @@ import { h } from './element';
 import Icon from './icon';
 import { cssPrefix } from '../config';
 
+const CLASS_NO_SCROLL = `${cssPrefix}-no-scroll`;
+
 export function xtoast(title, content) {
   const el = h('div', `${cssPrefix}-toast`);
   const dimmer = h('div', `${cssPrefix}-dimmer active`);
   const remove = () => {
     document.body.removeChild(el.el);
     document.body.removeChild(dimmer.el);
+    document.body.classList.remove(CLASS_NO_SCROLL);
   };
 
   el.children(
@@ -20,6 +23,7 @@ export function xtoast(title, content) {
   );
   document.body.appendChild(el.el);
   document.body.appendChild(dimmer.el);
+  document.body.classList.add(CLASS_NO_SCROLL);
   // set offset
   const { width, height } = el.box();
   const { clientHeight, clientWidth } = document.documentElement;
